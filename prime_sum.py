1966a21d33d219a1185a7796e444da1f59ef1073
Dodyr <55747629+Dodyr@users.noreply.github.com>
2021-12-07 22:57:43 +0300

    not working arrays

---
+++ prime_sum.py
@@ -0,0 +1,14 @@
+N = 2000000
+crossed =array(dtybe=bool)
+summ=0
+i = 2
+while i<N:
+    summ = summ + i
+    j = i*2
+    while j<N:
+        crossed[j]=True
+        j = j + i
+    i = i + 1
+    while i < N and crossed[i]:
+        i = i + 1
+print(sum)
